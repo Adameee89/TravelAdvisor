2f04e66e85079bfe5b8f841fd673be7fa0e27ada
Adam Vegvari <brokenskijas9@gmail.com>
2024-07-18 12:42:57 +0200

    Travel Advisor with Next.js/tailwind/rapidapi/googlemapsapi

---
+++ pages/api/index.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const url =
+  "https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary";
+
+export const getPlacesData = async (type, sw, ne) => {
+  try {
+    const {
+      data: { data },
+    } = await axios.get(
+      `https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,
+      {
+        params: {
+          bl_latitude: sw.lat,
+          tr_latitude: ne.lat,
+          bl_longitude: sw.lng,
+          tr_longitude: ne.lng,
+        },
+        headers: {
+          "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
+          "x-rapidapi-key": "4c6806186bmshdc5a602fb55b29dp18c21bjsnf96c5dc5ebe2",
+        },
+      }
+    );
+    return data;
+  } catch (error) {
+    console.log(`Fetch data Error : ${error}`);
+  }
+};
